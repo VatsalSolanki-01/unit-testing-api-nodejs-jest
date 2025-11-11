bfd6bc019ec8a9449bffc490a3c16f0f72ade513
VatsalSolanki-01 <solanki.vatsal009@gmail.com>
2025-11-11 12:45:00 +0530

    fix: files

--- tests/server.test.js
+++ tests/server.test.js
@@ -1,59 +1,31 @@
-const mongoose = require("mongoose");
-const { MongoMemoryServer } = require("mongodb-memory-server");
-const supertest = require("supertest");
 const app = require("../server");
+const supertest = require("supertest");
+const mockingoose = require("mockingoose");
 const Post = require("../models/Post");
 
-let mongoServer;
+describe("Mocked API Tests", () => {
+  beforeEach(() => {
+    mockingoose.resetAll();
+  });
 
-// Start in-memory MongoDB before all tests
-beforeAll(async () => {
-  mongoServer = await MongoMemoryServer.create();
-  const uri = mongoServer.getUri();
-  await mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-});
+  test("GET /api/posts", async () => {
+    const mockPosts = [{ _id: "507f1f77bcf86cd799439011", title: "Post 1", content: "Lorem ipsum" }];
 
-// Clear all collections after each test
-afterEach(async () => {
-  const collections = mongoose.connection.collections;
-  for (const key in collections) {
-    await collections[key].deleteMany({});
-  }
-});
+    mockingoose(Post).toReturn(mockPosts, "find");
 
-// Stop MongoDB and disconnect mongoose after all tests
-afterAll(async () => {
-  await mongoose.connection.close();
-  await mongoServer.stop();
-});
+    const response = await supertest(app).get("/api/posts").expect(200);
+    expect(Array.isArray(response.body)).toBe(true);
+    expect(response.body[0].title).toBe("Post 1");
+  });
 
-test("GET /api/posts", async () => {
-  const post = await Post.create({ title: "Post 1", content: "Lorem ipsum" });
+  test("POST /api/posts", async () => {
+    const data = { title: "New Post", content: "Hello world" };
+    const mockPost = { _id: "507f1f77bcf86cd799439012", ...data };
 
-  await supertest(app)
-    .get("/api/posts")
-    .expect(200)
-    .then((response) => {
-      expect(Array.isArray(response.body)).toBeTruthy();
-      expect(response.body.length).toBe(1);
-      expect(response.body[0].title).toBe(post.title);
-      expect(response.body[0].content).toBe(post.content);
-    });
-});
+    mockingoose(Post).toReturn(mockPost, "save");
 
-test("POST /api/posts", async () => {
-  const data = { title: "Post 1", content: "Lorem ipsum" };
-
-  await supertest(app)
-    .post("/api/posts")
-    .send(data)
-    .expect(200)
-    .then(async (response) => {
-      expect(response.body.title).toBe(data.title);
-      expect(response.body.content).toBe(data.content);
-
-      const post = await Post.findOne({ _id: response.body._id });
-      expect(post).toBeTruthy();
-      expect(post.title).toBe(data.title);
-    });
+    const response = await supertest(app).post("/api/posts").send(data).expect(200);
+    expect(response.body.title).toBe(data.title);
+    expect(response.body.content).toBe(data.content);
+  });
 });
