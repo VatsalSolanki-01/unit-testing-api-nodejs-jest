75f01b846707b1ffc11207501156c5d2d5fa2b80
VatsalSolanki-01 <solanki.vatsal009@gmail.com>
2025-11-11 12:36:28 +0530

    fix

--- tests/server.test.js
+++ tests/server.test.js
@@ -1,48 +1,59 @@
+const mongoose = require("mongoose");
+const { MongoMemoryServer } = require("mongodb-memory-server");
+const supertest = require("supertest");
 const app = require("../server");
 const Post = require("../models/Post");
-const mongoose = require("mongoose");
-const supertest = require("supertest");
 
-jest.setTimeout(30000); // increase timeout for slower Docker environment
+let mongoServer;
 
-const MONGO_URL = process.env.MONGO_URL || "mongodb://mongodb:27017/JestDB";
-
+// Start in-memory MongoDB before all tests
 beforeAll(async () => {
-  let connected = false;
-  for (let i = 0; i < 10 && !connected; i++) {
-    try {
-      await mongoose.connect(MONGO_URL, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      });
-      connected = true;
-    } catch (err) {
-      console.log("Waiting for MongoDB to be ready...");
-      await new Promise((res) => setTimeout(res, 3000));
-    }
-  }
-
-  if (!connected) throw new Error("Failed to connect to MongoDB");
+  mongoServer = await MongoMemoryServer.create();
+  const uri = mongoServer.getUri();
+  await mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 });
 
+// Clear all collections after each test
+afterEach(async () => {
+  const collections = mongoose.connection.collections;
+  for (const key in collections) {
+    await collections[key].deleteMany({});
+  }
+});
+
+// Stop MongoDB and disconnect mongoose after all tests
 afterAll(async () => {
-  await mongoose.connection.db.dropDatabase();
   await mongoose.connection.close();
+  await mongoServer.stop();
+});
+
+test("GET /api/posts", async () => {
+  const post = await Post.create({ title: "Post 1", content: "Lorem ipsum" });
+
+  await supertest(app)
+    .get("/api/posts")
+    .expect(200)
+    .then((response) => {
+      expect(Array.isArray(response.body)).toBeTruthy();
+      expect(response.body.length).toBe(1);
+      expect(response.body[0].title).toBe(post.title);
+      expect(response.body[0].content).toBe(post.content);
+    });
 });
 
 test("POST /api/posts", async () => {
   const data = { title: "Post 1", content: "Lorem ipsum" };
 
-  const response = await supertest(app).post("/api/posts").send(data).expect(200);
+  await supertest(app)
+    .post("/api/posts")
+    .send(data)
+    .expect(200)
+    .then(async (response) => {
+      expect(response.body.title).toBe(data.title);
+      expect(response.body.content).toBe(data.content);
 
-  expect(response.body.title).toBe(data.title);
-  expect(response.body.content).toBe(data.content);
-});
-
-test("GET /api/posts", async () => {
-  await Post.create({ title: "Post 1", content: "Lorem ipsum" });
-
-  const response = await supertest(app).get("/api/posts").expect(200);
-  expect(Array.isArray(response.body)).toBeTruthy();
-  expect(response.body.length).toBeGreaterThan(0);
+      const post = await Post.findOne({ _id: response.body._id });
+      expect(post).toBeTruthy();
+      expect(post.title).toBe(data.title);
+    });
 });
